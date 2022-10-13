0daa4c8998088c415e92131f720b8f0ca88e1741
Justin Haddock <jhadddock@protonmail.com>
2022-10-13 11:22:34 -0700

    Security headers added

--- API/Startup.cs
+++ API/Startup.cs
@@ -59,11 +59,34 @@ namespace API
         {
 
             app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opt => opt.NoReferrer());
+            app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+            app.UseXfo(opt => opt.Deny());
+            app.UseCsp(opt => opt
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://cdn.jsdelivr.net"))
+                .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:", "https://cdn.jsdelivr.net"))
+                .FormActions(s => s.Self())
+                .FrameAncestors(s =>s.Self())
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ScriptSources( s => s.Self())
+            );
+
             if (env.IsDevelopment())
             {
                 
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+            }
+
+            else {
+                app.Use(async (context, next) =>
+                {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                    await next.Invoke();
+                });
             }
 
             //app.UseHttpsRedirection();
