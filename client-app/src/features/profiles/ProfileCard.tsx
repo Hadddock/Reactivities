60f51e10d4d92b01488037dab7516daf0a08b9d5
Justin Haddock <jhadddock@protonmail.com>
2022-10-18 12:18:18 -0700

    Fixed typo causing follwers to display without default image when viewed from profile tab

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -18,7 +18,7 @@ export default observer(function ProfileCard({ profile }: Props) {
     }
     return (
         <Card as= {Link} to= { `/profiles/${profile.username}` }>
-            <Image src={profile.image || 'assets/user.png'} />
+            <Image src={profile.image || '/assets/user.png'} />
             <Card.Content>
                 <Card.Header> {profile.displayName} </Card.Header>
                 <Card.Description>{truncate(profile.bio)}</Card.Description>
