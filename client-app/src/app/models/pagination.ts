4fbc1dfef1f67e30292a3438265b48d8bd20b2dc
Justin Haddock <jhadddock@protonmail.com>
2022-10-18 13:36:25 -0700

    Fixed Profile

--- client-app/src/app/models/pagination.ts
+++ client-app/src/app/models/pagination.ts
@@ -20,7 +20,7 @@ export class PaginatedResult<T>  {
 export class PagingParams {
     pageNumber ;
     pageSize;
-    constructor(pageNumber = 1, pageSize = 2) {
+    constructor(pageNumber = 1, pageSize = 4) {
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
     }
