a10c293335ddc868ea6024b3a0dd0d83422744ff
Justin Haddock <jhadddock@protonmail.com>
2023-01-27 16:33:46 -0800

    Fix password registration regex

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,7 +10,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,100}$", ErrorMessage = "Password must be complex. (at least 8 characters, 1 lowercase letter, 1 uppercase letter)")]
         public string Password { get; set; }
         [Required]
         public string Username { get; set; }
