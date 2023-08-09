dcac6eb5979e568bd56ba3a7c7d5abb73edabef5
Justin Haddock <jhadddock@protonmail.com>
2023-08-08 19:08:44 -0700

    Add email verification message to register form

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,41 +1,60 @@
-import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
-import { useStore } from '../../app/stores/store';
-import LoginForm from '../users/LoginForm';
-import RegisterForm from '../users/RegisterForm';
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
 export default observer(function HomePage() {
-    const { userStore, modalStore } = useStore();
-    return (
-        <Segment inverted textAlign='center' vertical className='masthead'>
-            <Container text>
-                <Header as='h1' inverted>
+  const { userStore, modalStore } = useStore();
+  return (
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          Reactivities
+        </Header>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header as="h2" inverted content="Welcome to Reactivities" />
 
-                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
-                    Reactivities
-                </Header>
-                {userStore.isLoggedIn ? (
-                    <>
-                        <Header as='h2' inverted content='Welcome to Reactivities' />
-                        <Button as={Link} to='/activities' size='huge' inverted>
-                            Go To Activities
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        <Button onClick= {() => modalStore.openModal(<LoginForm/>)} to='/login' size='huge' inverted>
-                            Login
-                        </Button>
-                        <Button onClick= {() => modalStore.openModal(<RegisterForm/>)} to='/login' size='huge' inverted>
-                            Register
-                        </Button>
-                    </>
-                )}
+            <Button as={Link} to="/activities" size="huge" inverted>
+              Go To Activities
+            </Button>
+          </>
+        ) : (
+          <>
+            <Header
+              as="h3"
+              inverted
+              content="Visiting from my portfolio? Feel free to register with a fake email address."
+            />
 
-
-            </Container>
-        </Segment>
-    )
-})
+            <Button
+              onClick={() => modalStore.openModal(<LoginForm />)}
+              to="/login"
+              size="huge"
+              inverted
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => modalStore.openModal(<RegisterForm />)}
+              to="/login"
+              size="huge"
+              inverted
+            >
+              Register
+            </Button>
+          </>
+        )}
+      </Container>
+    </Segment>
+  );
+});
