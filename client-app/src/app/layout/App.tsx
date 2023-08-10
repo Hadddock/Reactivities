ebedca85ebecf7ef7a63e12fd1a6fc2b0246ccf3
Justin Haddock <jhadddock@protonmail.com>
2023-08-09 18:58:40 -0700

    Disable errors page direct link

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,60 +1,74 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import { Container } from 'semantic-ui-react';
+import { Container } from "semantic-ui-react";
 
-import NavBar from './NavBar';
+import NavBar from "./NavBar";
 
-import { observer } from 'mobx-react-lite';
-import { Route, Switch, useLocation } from 'react-router-dom';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import HomePage from '../../features/home/HomePage';
-import ActivityForm from '../../features/activities/form/ActivityForm';
-import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import TestErrors from '../../features/errors/TestError';
-import { ToastContainer } from 'react-toastify';
-import NotFound from '../../features/errors/NotFound';
-import ServerError from '../../features/errors/ServerError';
-import { useStore } from '../stores/store';
-import LoadingComponent from './LoadingComponents';
-import ModalContainer from '../common/modals/ModalContainer';
-import ProfilePage from '../../features/profiles/ProfilePage';
-import PrivateRoute from './PrivateRoute';
+import { observer } from "mobx-react-lite";
+import { Route, Switch, useLocation } from "react-router-dom";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import HomePage from "../../features/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
+import { ToastContainer } from "react-toastify";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
+import { useStore } from "../stores/store";
+import LoadingComponent from "./LoadingComponents";
+import ModalContainer from "../common/modals/ModalContainer";
+import ProfilePage from "../../features/profiles/ProfilePage";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
-
   const location = useLocation();
   const { commonStore, userStore } = useStore();
-  
+
   useEffect(() => {
     if (commonStore.token) {
       userStore.getUser().finally(() => commonStore.setAppLoaded());
     } else {
       commonStore.setAppLoaded();
     }
-  }, [commonStore, userStore])
+  }, [commonStore, userStore]);
 
-  if(!commonStore.appLoaded) return <LoadingComponent content = 'Loading app...'/>
+  if (!commonStore.appLoaded)
+    return <LoadingComponent content="Loading app..." />;
 
   return (
     <>
-      <ToastContainer position='bottom-right' hideProgressBar />
-      <ModalContainer/>
-      <Route exact path='/' component={HomePage} />
+      <ToastContainer position="bottom-right" hideProgressBar />
+      <ModalContainer />
+      <Route exact path="/" component={HomePage} />
       <Route
-        path = {'/(.+)'}
-        render= {() => (
+        path={"/(.+)"}
+        render={() => (
           <>
             <NavBar />
-            <Container style={{ marginTop: '7em' }}>
+            <Container style={{ marginTop: "7em" }}>
               <Route>
                 <Switch>
-                  <Route exact path='/' component={HomePage} />
-                  <PrivateRoute exact path='/activities' component={ActivityDashboard} />
-                  <PrivateRoute path='/activities/:id' component={ActivityDetails} />
-                  <PrivateRoute key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                  <PrivateRoute path='/profiles/:username' component = {ProfilePage}/>
-                  <PrivateRoute path='/errors' component={TestErrors} />
-                  <Route path='/server-error' component={ServerError} />
+                  <Route exact path="/" component={HomePage} />
+                  <PrivateRoute
+                    exact
+                    path="/activities"
+                    component={ActivityDashboard}
+                  />
+                  <PrivateRoute
+                    path="/activities/:id"
+                    component={ActivityDetails}
+                  />
+                  <PrivateRoute
+                    key={location.key}
+                    path={["/createActivity", "/manage/:id"]}
+                    component={ActivityForm}
+                  />
+                  <PrivateRoute
+                    path="/profiles/:username"
+                    component={ProfilePage}
+                  />
+                  {/* <PrivateRoute path='/errors' component={TestErrors} /> */}
+                  <Route path="/server-error" component={ServerError} />
                   <Route component={NotFound} />
                 </Switch>
               </Route>
@@ -62,7 +76,6 @@ function App() {
           </>
         )}
       />
-
     </>
   );
 }
